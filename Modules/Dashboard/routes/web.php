4562248a2752cf13555d40cc0b00cf0e84b5869b
Javier Tapia <javier.tapia.d@gmail.com>
2025-07-31 18:28:02 -0400

    [CHG] Dashboard: move routing to module

---
+++ Modules/Dashboard/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'verified'])
+    ->prefix('dashboard')
+    ->group(function () {
+        Route::view('/', 'dashboard::dashboard')->name('dashboard');
+
+        Route::redirect('settings', 'settings/profile');
+        Route::get('settings/profile', Profile::class)->name('settings.profile');
+        Route::get('settings/password', Password::class)->name('settings.password');
+        Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    });
+
