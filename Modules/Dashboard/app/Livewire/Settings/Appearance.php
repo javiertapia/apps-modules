856309351b82029493d72e2f115ed635692d03eb
Javier Tapia <javier.tapia.d@gmail.com>
2025-07-31 19:00:46 -0400

    [FIX] Dashboard: imported class

--- Modules/Dashboard/app/Livewire/Settings/Appearance.php
+++ Modules/Dashboard/app/Livewire/Settings/Appearance.php
@@ -1,12 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Dashboard\Livewire\Settings;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Appearance extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('dashboard::livewire.settings.appearance');
     }
